817f0f76d67cf0a6285fd6a88ab7335dcef37952
Godknows-Jv <nyandorog781@gmail.com>
2024-08-21 12:54:45 +0200

    Add files via upload

---
+++ car_sales/javascript.js
@@ -0,0 +1,19 @@
+
+  //  DETAILS
+   // GODKNOWS NYANDORO
+   // R237561Y
+   // LEVEL 1.2
+   //  HAI
+
+var MenuItems = document.getElementById("MenuItems");
+MenuItems.style.maxHeight = "0px";
+function menutoggle() {
+    if(MenuItems.style.maxHeight=="0px")
+    {
+        MenuItems.style.maxHeight = "200px"
+    }
+    else 
+    {
+        MenuItems.style.maxHeight="0px"
+    }
+}
